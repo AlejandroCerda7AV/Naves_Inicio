bcc738f104770079482c1f56fedf1c3ddcdc3da0
Vladimir <viperezf@upbc.edu.mx>
2014-10-17 10:42:27 -0700

    Actividad 3: Revisada

--- Ejecutable/Readme.txt
+++ Ejecutable/Readme.txt
@@ -2,3 +2,4 @@ listo maestro.[AlejandroCerda]
 
 Actividad 1: 10 Bien.
 Actividad 2: 9 Bien.
+Actividad 3: 7 Falto el Metodo iniciando(), esta llamado mal, se tenia codigo extra.
--- Source/Game.cpp
+++ Source/Game.cpp
@@ -6,25 +6,41 @@
 
 
 CGame::CGame(){
+	//ACT3: Mal, les pedi que esto lo pusieran en el metodo "Iniciando()".
+	//estado;
+	//SDL_Surface*screen;
+	//if (SDL_Init(SDL_INIT_VIDEO)){
+	//	printf("Error: %s", SDL_GetError());
+	//	exit(EXIT_FAILURE);
+	//}
 
-	estado;
-	SDL_Surface*screen;
-	if (SDL_Init(SDL_INIT_VIDEO)){
-		printf("Error: %s", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
-	
-		screen=SDL_SetVideoMode(640, 480, 24, SDL_SWSURFACE);
-	if (screen==NULL){
-		printf("Error: %s", SDL_GetError());
-		exit(EXIT_FAILURE);
-	}
-	SDL_Flip(screen);
-	SDL_WM_SetCaption("Mi primer juego", NULL);
+	//	screen=SDL_SetVideoMode(640, 480, 24, SDL_SWSURFACE);
+	//if (screen==NULL){
+	//	printf("Error: %s", SDL_GetError());
+	//	exit(EXIT_FAILURE);
+	//}
+	//SDL_Flip(screen);
+	//SDL_WM_SetCaption("Mi primer juego", NULL);
 	atexit(SDL_Quit);
 
 	estado= ESTADO_INICIANDO;//ACT2: Mal, aqui debes de indicar tu estado inicial.
 
+}
+
+//ACT3: Mal, falto agregar este metodo
+void CGame::Iniciando(){
+	if (SDL_Init(SDL_INIT_VIDEO)){
+		printf("Error: %s", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	screen=SDL_SetVideoMode(640, 480, 24, SDL_SWSURFACE);
+	if (screen==NULL){
+		printf("Error: %s", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+
+	SDL_WM_SetCaption("Mi primer juego", NULL);
 }
 
 // Con esta función eliminaremos todos los elementos en pantalla
@@ -36,22 +52,23 @@ bool CGame::Start()
 {
 	// Esta variable nos ayudara a controlar la salida del juego...
 	int salirJuego = false;
-          
+
 	while (salirJuego == false){
-            
+
 		//Maquina de estados
 		switch(estado){
 		case  Estado::ESTADO_INICIANDO:
-			void Iniciando();
-			{
-				//screen=SDL_SetVideoMode
-				nave = IMG_LoadJPG_RW(SDL_RWFromFile("../Data/umi.jpg","rb"));
-
-				SDL_BlitSurface(nave, NULL, screen, NULL);
-				SDL_FreeSurface(nave);
-			}
+			estado = ESTADO_MENU;
+			Iniciando();//ACT3: Esta llamada estaba mal
 			break;
 		case 	Estado::ESTADO_MENU:
+
+			//screen=SDL_SetVideoMode
+			nave = IMG_LoadJPG_RW(SDL_RWFromFile("../Data/umi.jpg","rb"));
+
+			SDL_BlitSurface(nave, NULL, screen, NULL);
+			SDL_FreeSurface(nave);
+
 			break;
 		case Estado::ESTADO_JUGANDO:
 			break;
@@ -63,6 +80,6 @@ bool CGame::Start()
 			salirJuego = true;
 			break;
 		};
-    }
+	}
 	return true;
 }
